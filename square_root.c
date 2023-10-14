3a9c3b6c3bf74128ab2fe4a8fdfd4c67bad8e436
Paritosh Kolwadkar <paritoshkolwadkar@Paritoshs-MacBook-Air.local>
2023-10-14 13:10:36 +0530

    hello

---
+++ square_root.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+double fun2x(double x){
+    return 2*x;
+}
+double fun(double x, double num){
+    return x*x-num;
+}
+double sqarert(double num){
+    double =1.0;
+    double result;
+    double error=result-x;
+    int x=result;
+}
+int main(){
+    
+return 0;
+}
